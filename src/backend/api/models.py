225dafbc8ab9d1865aa0e721973210a082c4efaf
Thallys <thallysvilemos@gmail.com>
2024-11-17 23:57:39 -0300

    feat: inclusão de script de migração

--- src/backend/api/models.py
+++ src/backend/api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+class MacroRegiao(models.Model):
+    nome = models.CharField(max_length=100)
+
+    class Meta:
+        app_label = 'api'
+
+    def __str__(self):
+        return self.nome
+
+class RegiaoSaude(models.Model):
+    nome = models.CharField(max_length=100)
+    macro_regiao = models.ForeignKey(MacroRegiao, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'api'
+
+    def __str__(self):
+        return self.nome
+
+class Cidade(models.Model):
+    codigo_ibge = models.CharField(max_length=7, unique=True)
+    nome = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    regiao_saude = models.ForeignKey(RegiaoSaude, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        app_label = 'api'
+
+    def __str__(self):
+        return f"{self.nome} ({self.codigo_ibge})"
+
+class Indicador(models.Model):
+    nome_arquivo = models.CharField(max_length=100, unique=True)
+    titulo = models.TextField()
+    subtitulo = models.TextField(null=True, blank=True)
+    fonte = models.TextField()
+
+    class Meta:
+        app_label = 'api'
+
+    def __str__(self):
+        return self.titulo
+
+class ValorIndicador(models.Model):
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+    indicador = models.ForeignKey(Indicador, on_delete=models.CASCADE)
+    ano = models.IntegerField()
+    valor = models.FloatField(null=True)
+
+    class Meta:
+        app_label = 'api'
+        unique_together = ['cidade', 'indicador', 'ano']
+
+    def __str__(self):
+        return f"{self.cidade} - {self.indicador} ({self.ano}): {self.valor}"
